af3e5a6f8a28a9cccbafe88d97f0ba56f12c8212
sebasped <sebasped@disroot.org>
2020-02-07 10:43:04 -0300

    mini detaller

--- definicionYalgoMas.py
+++ definicionYalgoMas.py
@@ -8,9 +8,8 @@ def dormir():
 
 # entonces para que esto no se ejecute cuando importan dormir()
 # hago así
-
 if __name__ == '__main__':
     print('Ahora esto no se ejecuta cuando corro main.py')
 
-# pueden probar de comentar el if (e identar bien la siguiente línea)
+# pueden probar de comentar el if (e indentar bien la siguiente línea)
 # y volver a correr main.py, como para ver la diferencia
