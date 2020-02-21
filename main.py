6f0e330ce83ad59073f05d25dae4ef0f319be9cb
sebasped <sebasped@disroot.org>
2020-02-21 11:22:35 -0300

    agregado importar desde otro directorio

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from definicionYalgoMas import dormir
 
+# como el directorio tiene un archivo __init__.py entonces puedo importar desde ahí
+# sería from [path_relativo].[archivo sin .py] import [clase o función]
+from clases.tiempo import Contador
+
 # ahora cuando ejecuto este archivo, lo que está en definicionYalgoMas.py NO se ejecuta
 dormir()
